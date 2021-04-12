e10843c0630b45cb7a08cf8e7bdb23d0e5abe46d
Yasir Ahmed Siddiqui <yasirahmedsidd@gmail.com>
2021-04-12 12:02:17 +0500

    unit * price calculation completed

--- src/screens/Billing.js
+++ src/screens/Billing.js
@@ -1,16 +1,45 @@
 import {useNavigation} from '@react-navigation/core';
 import React, {useState} from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-import {TextInput} from 'react-native-paper';
+import {Text, TouchableOpacity, View, ScrollView} from 'react-native';
+import {TextInput, DataTable} from 'react-native-paper';
 import Header from '../components/Header';
 const Billing = ({slabs}) => {
   const {goBack} = useNavigation();
   const [reading, setReading] = useState('');
+  const [totalUnits, setTotalUnits] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  const calculateBilling = value => {
+    const setValue = val => (value = val);
+    let totalUnits = [];
+    let totalAmount = 0;
+
+    for (let i = 0; i < slabs.length; i++) {
+      let x = value - slabs[i].end;
+      if (slabs[i].end === null) {
+        totalUnits.push({units: value, rate: slabs[i].rate});
+        break;
+      }
+      if (x <= 0) {
+        totalUnits.push({units: value, rate: slabs[i].rate});
+        break;
+      } else if (x >= 0) {
+        setValue(value - slabs[i].end);
+        totalUnits.push({units: slabs[i].end, rate: slabs[i].rate});
+      }
+    }
+    setTotalUnits(totalUnits);
+
+    for (let i = 0; i < totalUnits.length; i++) {
+      totalAmount += totalUnits[i].units * totalUnits[i].rate;
+    }
+    setTotalAmount(totalAmount);
+  };
   return (
-    <>
+    <ScrollView>
       <Header callback={goBack} />
-      <Text>{JSON.stringify(slabs)}</Text>
-      <View>
+
+      <View style={{marginTop: 36, width: '90%', alignSelf: 'center'}}>
         <TextInput
           mode="outlined"
           value={reading}
@@ -21,11 +50,11 @@ const Billing = ({slabs}) => {
         <TouchableOpacity
           style={{
             alignSelf: 'center',
-            marginTop: 36,
+            marginVertical: 36,
             alignItems: 'center',
           }}
           onPress={() => {
-            console.log(reading);
+            calculateBilling(reading);
           }}>
           <Text
             style={{
@@ -41,10 +70,34 @@ const Billing = ({slabs}) => {
         </TouchableOpacity>
         {/* Details */}
         <View>
-          <Text>Your Units: {reading}</Text>
+          {totalUnits.length !== 0 && (
+            <DataTable>
+              <DataTable.Header>
+                <DataTable.Title>Units</DataTable.Title>
+                <DataTable.Title>Rate</DataTable.Title>
+              </DataTable.Header>
+              {totalUnits.map((t, index) => (
+                <DataTable.Row key={index}>
+                  <DataTable.Cell>{`${t.units} units`}</DataTable.Cell>
+                  <DataTable.Cell>{`@ ${t.rate} / unit`}</DataTable.Cell>
+                </DataTable.Row>
+              ))}
+            </DataTable>
+          )}
+          <View
+            style={{
+              marginTop: 36,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            {/* <Text>Units: {reading}</Text>
+            <Text>Your Readings: {JSON.stringify(totalUnits)}</Text> */}
+            <Text style={{fontSize: 32}}>Total Amount</Text>
+            <Text style={{fontSize: 32,fontWeight:"bold"}}>{totalAmount}</Text>
+          </View>
         </View>
       </View>
-    </>
+    </ScrollView>
   );
 };
 
